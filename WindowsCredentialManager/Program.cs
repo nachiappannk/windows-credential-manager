9e6c13be0b886f4882f1c83f458244509a320e58
Nachiappan <nachiapan@gmail.com>
2023-06-21 07:30:33 +0530

    first commit

---
+++ WindowsCredentialManager/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AdysTech.CredentialManager;
+using System.Net;
+
+Console.WriteLine("Hello, World!");
+var cred = new NetworkCredential("userid", "Password");
+CredentialManager.SaveCredentials("testnachicreds", cred);
+
+var credRetrieved = CredentialManager.GetCredentials("testnachicreds");
+Console.WriteLine(credRetrieved.UserName);
+Console.WriteLine(credRetrieved.Password);
+
